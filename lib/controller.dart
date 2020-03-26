20aa3685254e8b13875358eeb33727abf72e083a
Haroldo <haroldobeyerbacellar@gmail.com>
2020-03-26 13:27:20 -0300

    aula 1

---
+++ lib/controller.dart
@@ -0,0 +1,22 @@
+import 'package:mobx/mobx.dart';
+
+class Controller {
+  var _counter = Observable(0);
+
+  int get counter => _counter.value;
+  set counter(int newValue) => _counter.value = newValue;
+
+  Action increment;
+
+  Controller() {
+    increment = Action(_incroment);
+
+    autorun((_) {
+      print(counter);
+    });
+  }
+
+  _incroment() {
+    counter++;
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_1/controller.dart';
+
+class MyHomePage extends StatelessWidget {
+  final controller = Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MOBX TESTE'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Observer(
+              builder: (_) {
+                return Text('${controller.counter}',
+                    style: Theme.of(context).textTheme.display1);
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
