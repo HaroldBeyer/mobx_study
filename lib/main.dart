c59fb85169b1469e4601b6cd20d41b369072e4f2
Haroldo <haroldobeyerbacellar@gmail.com>
2020-03-27 18:04:55 -0300

    aula 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:mobx_1/controller.dart';
 import 'package:mobx_1/home.dart';
-import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  GetIt getIt = GetIt.I;
+  getIt.registerSingleton<Controller>(Controller());
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          Provider<Controller>(
-            create: (_) => Controller(),
-            dispose: (_, controler) => controler.dispose(),
-          )
-        ],
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: MyHomePage(),
-        ));
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
   }
 }
